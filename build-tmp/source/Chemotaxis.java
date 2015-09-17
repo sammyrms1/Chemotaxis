a098bc426ddb742a60b7aa2e5f26de11e8ec0930
sammyrms1 <super.sammy2011@gmail.com>
2015-09-17 11:28:25 -0700

    taxis

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria sRollins;
 Bacteria sSting;
 Bacteria [] fans;
 Bacteria [] fans2;
-void setup()
+public void setup()
  {     
  	size(640, 480);
  	sRollins = new Bacteria (200, 200, 50);
@@ -15,7 +31,7 @@ void setup()
  	}
  }
 
-void draw()
+public void draw()
  {
  	background(0);
  	sRollins.move();
@@ -40,15 +56,24 @@ class Bacteria
 		bacCol = col;
 	}
 
-	void move()
+	public void move()
 	{
 		bacX += (int)(Math.random()*5)-2;
 		bacY += (int)(Math.random()*5)-2;
 	}
 
-	void show()
+	public void show()
 	{
 		fill(bacCol);
 		ellipse(bacX, bacY, 20, 20);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
